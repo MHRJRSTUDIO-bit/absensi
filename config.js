952394d9f6fbb501b58205bb8cdbf97b22ef7121
MHRJRSTUDIO-bit <ranggastm70@gmail.com>
2026-09-21 17:02:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// GANTI kedua nilai ini setelah membuat project Supabase.
-window.SUPABASE_URL = "https://tyrtweiupblztlevardv.supabase.co/rest/v1/";
-window.SUPABASE_ANON_KEY = "sb_publishable_Ia6xoHAQsvr3LTA-l1rKOQ_clvd43fY";
+// GANTI kedua nilai ini setelah membuat project Supabase
+window.SUPABASE_URL = "https://tyrtweiupblztlevardv.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_6Ia6xoHAQsvr3LTA-l1rK0Q_clvd43fY";
